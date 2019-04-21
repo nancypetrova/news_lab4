b3e1433bb1b2501a761408c71c65e989648bf388
nancypetrova <nencypetrova@mail.ru>
2019-04-21 22:49:59 +0300

    3

--- article.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once 'database.php' ;
-    require_once 'models/articles.php' ;
-
-    $link = mysqli_connect($db_host, $db_user, $db_password, $db_name);
-    $article = articles_get($link, $_GET['id']);
-
-    include("views/article.php")
-?>
